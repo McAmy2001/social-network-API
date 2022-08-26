b0b6dba4bb36b1fa91220b2024ab31f389f693e3
Amy McCabe <mcca0168@umn.edu>
2022-08-26 15:39:31 -0500

    server.js, models started

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const validateEmail = require('../utils/validateEmail');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [validateEmail, 'Please enter a valid email address.']
+  },
+  thoughts: [],
+  friends: []
+});
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+//app.use(require('/routes'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/social-network-API', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost ${PORT}`));
